b3a3cec34130535f97577d250b1f34901ac1b04a
wolfayoub2023 <125779865+wolfayoub2023@users.noreply.github.com>
2023-11-07 21:46:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from characterai import PyAsyncCAI
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
